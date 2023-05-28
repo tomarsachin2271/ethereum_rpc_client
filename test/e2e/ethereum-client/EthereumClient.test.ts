383adc8f744c631eddcb5453ef8976f3159ceebc
proxima424 <darpitrangari42@gmail.com>
2023-05-28 19:43:32 +0530

    WOWWW

--- test/e2e/ethereum-client/EthereumClient.test.ts
+++ test/e2e/ethereum-client/EthereumClient.test.ts
@@ -1,9 +1,13 @@
 import { EthereumClient } from "../../../src";
-import networkConfig from "./networkConfig";
+import {networkConfig,uniNetworkConfig} from "./networkConfig";
 
 const testAddress = '0x229F701345b239c9C0a0685ef32e34842eDd1a45';
 // truncated non-20 bytes string
 const invalidTestAddress = '0x229F701345b239c9C0a0685ef32e34842';
+//
+const univ3factoryAddress = '0x1F98431c8aD98523631AE4a59f267346ea31F984';
+//
+const EOAaddress = '0x1fD3Acf96F8B14994Fb912e2330641884dA00D6E';
 
 describe('EthereumClient E2E tests', () => {
     networkConfig.forEach(({ nodeUrls }) => {
@@ -31,10 +35,49 @@ describe('EthereumClient E2E tests', () => {
             it('should throw an error when getTransactionCount method is provided with an invalid address', async()=>{
                 const address = invalidTestAddress;
                 await expect(client.getTransactionCount(address)).rejects.toThrow(Error);
+            });
+
+            it('should call the getGasPrice method and return the result', async() =>{
+                const gasPrice = await client.getGasPrice();
+                expect(typeof gasPrice).toBe('string');
+                const numGasPrice = parseInt(gasPrice,16);
+                expect(numGasPrice).toBeGreaterThanOrEqual(0);
+            })
+
+            it('should call the getMaxPriorityFeePerGas method and return the result', async()=>{
+                const maxPriorityFeePerGas = await client.getMaxPriorityFeePerGas();
+                expect(typeof maxPriorityFeePerGas).toBe('string');
+                const numMaxPriorityFeePerGas = parseInt(maxPriorityFeePerGas,16);
+                expect(numMaxPriorityFeePerGas).toBeGreaterThanOrEqual(0);
             })
 
             // Add more tests as needed
         });
 
     });
+    uniNetworkConfig.forEach(({ nodeUrls })=>{
+        describe(`Ethereum E2E state tests for ${nodeUrls}`,()=>{
+            const client = new EthereumClient({defaultNodeUrls: nodeUrls});
+
+            it('should call the getCode method and return the result', async()=>{
+                const address = univ3factoryAddress;
+                const code = await client.getCode(address);
+                expect(typeof code).toBe('string');
+            });
+
+            it('should call the getCode method and return 0x for EOA address', async()=>{
+                const address = EOAaddress;
+                const code = await client.getCode(address);
+                const expected = '0x';
+                expect(code).toBe(expected);
+            });
+
+            it('should call the getCode method and return error for invalid non 20 byte address', async()=>{
+                const address = invalidTestAddress;
+                await expect(client.getCode(address)).rejects.toThrow(Error);
+            });
+
+
+        })
+    })
 });
---
+++ test/e2e/ethereum-client/testsDetails.md
@@ -0,0 +1,16 @@
+### getCode (DONE)
+ 1] valid contract address, expect result type as string
+ 2] invalid contract address, non 20-bytes
+ 3] Pass in EOA and return zero
+
+### getGasPrice (DONE)
+ 1] Assert that the returned gas price is positive
+ 2] ( If a specific block number is also provided as an input in the upcoming versions, match the retreived gas
+    value to a historic value)
+
+### getMaxPriorityFeePerGas (DONE)
+ 1] Assert that the returned maximum priority fee per gas is a positive number.
+
+### estimateGas
+
+### getBlockByNumber
